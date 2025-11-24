48fe89e63f299b1b21fc7bad91d8eddeac91ebaa
Samoney2005 <samone.a.cook-1@ou.edu>
2025-11-23 18:31:44 -0600

    new changes

--- Python Class/11-20-2025_bank account.py
+++ Python Class/11-20-2025_bank account.py
@@ -1,32 +1,28 @@
-'''
-Create a class "BankAccount" 
+# Create a class "BankAccount"
+class BankAccount
+# Create a constructor with 2 parameters (owner and balance ) to create a bank account object
+def __init__(self,owner,balance):
+# Create a method called "balance" to show the balance of the account
 
-Create a constructor with 2 parameters (owner and balance ) to create a bank account object
+# Create a method called "deposit" to make a deposit with the amount in parameter. If the deposit is inferior to zero, print "Deposit must be positive"
 
-Create a method called "balance" to show the balance of the account
+# Create a method called "withdraw" to make a withdraw with the amount in the parameter. If the withdraw is superior to the balance, print "Insufficient funds".
 
-Create a method called "deposit" to make a deposit with the amount in parameter. If the deposit is inferior to zero, print "Deposit must be positive"
+# infinite loop
 
-Create a method called "withdraw" to make a withdraw with the amount in the parameter. If the withdraw is superior to the balance, print "Insufficient funds".
+	# print a Menu
 
+	# 1 - Check balance
+	# 2 - Deposit
+	# 3 - Withdraw
+	# 4 - Logout
 
-infinite loop
+	# enter your choice
 
-	print a Menu
-
-	1 - Check balance
-	2 - Deposit
-	3 - Withdraw
-	4 - Logout
-
-	enter your choice
-
-	if choice equal 1, Make a check balance
-	if choice equal 2, Make a deposit
-	if choice equals 3, make a withdraw
-	i choice equals 4, logout
+	# if choice equal 1, Make a check balance
+	# if choice equal 2, Make a deposit
+	# if choice equals 3, make a withdraw
+	# if choice equals 4, logout
+# else, print(invalid choice!)
 
 
-	else, print(invalid choice!)
-
-'''
