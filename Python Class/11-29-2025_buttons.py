7ba5ce0147d9edceb5ec4e96f11f1f5230fdbd9d
Samoney2005 <samone.a.cook-1@ou.edu>
2025-11-29 07:53:32 -0600

    new changes

---
+++ Python Class/11-29-2025_buttons.py
@@ -0,0 +1,13 @@
+from tkinter import *
+
+root = Tk()
+# change size of window
+root.geometry("100x100")
+
+button = Button (root, text="Click")
+
+# connect the button to the object
+button.pack(pady=15, padx=20)
+
+# keeps window open
+window.mainloop()
