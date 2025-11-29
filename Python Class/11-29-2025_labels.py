75175359febb0db7a678c5c43c1e477f1da273bc
Samoney2005 <samone.a.cook-1@ou.edu>
2025-11-29 07:46:22 -0600

    new changes

---
+++ Python Class/11-29-2025_labels.py
@@ -0,0 +1,10 @@
+from tkinter import *
+root = Tk()
+#changes the size of window
+root.geometry("100x100")
+#add a label to the window
+w = Label(root, text = "GUI")
+# connect the label to the window object "root in our case"
+w.pack()
+# keeps window open
+window.mainloop()
---
+++ Python Class/11-29-2025_windows.py
@@ -0,0 +1,4 @@
+import tkinter
+#Create an object caller window
+window - tkinter.Tk()
+window.mainloop()
