b566f0e735a9f7df51464d94d62ecdd2b32f3b62
Samoney2005 <samone.a.cook-1@ou.edu>
2026-01-02 10:59:47 -0600

    new changes

---
+++ Python Class/12-19-2025_get_data_in_column.py
@@ -0,0 +1,46 @@
+import pandas
+
+
+data = pandas.read_csv("weather_data.csv")
+
+#convert data from pandas to a dictionnary
+# Way 1: longer code without Pandas
+data_list = (data["temp"]).to_list()
+
+len_list = (len(data_list))
+sum_list = sum(data_list)
+
+result = sum_list/len_list
+print(result)
+
+
+#average of a series with pandas "mean()"
+# Way 2: With Pandas finding the average
+print(f'The mean is: {data["temp"].mean()}')
+
+# This will give the max
+print(f'The max is: {data["temp"].max()}')
+
+# simplify how to work with columns
+print(data["temp"])
+print(data.temp)
+
+#How to work with rows
+
+print(data[data["day"]=="Wednesday"])
+
+print(data[data["temp"]== 12])
+
+# Practice figure out how to get the mode:
+
+# Way 3: Another way but just the sum not the average
+'''
+sum=0
+
+
+for list in data_list:
+
+    sum+=list # sum = sum + list
+
+print(sum)
+'''
